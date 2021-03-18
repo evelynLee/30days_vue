a7de1bc2ed672d97322acecb846141fa97dd0164
Evelyn Lee <evelynlee8@gmail.com>
2021-03-18 15:38:53 +0800

    modify 2

--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { BootstrapVue } from 'bootstrap-vue'
 
+// Install BootstrapVue
+Vue.use(BootstrapVue)
 createApp(App).use(store).use(router).mount('#app')
