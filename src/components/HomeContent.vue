246449276bc7ac1c1eb4f54d41fc4595455cb418
Evelyn Lee <evelynlee8@gmail.com>
2021-03-18 17:15:52 +0800

    it works

---
+++ src/components/HomeContent.vue
@@ -0,0 +1,19 @@
+<template>
+    <p>This is HomeContent Component</p>
+</template>
+
+<script>
+export default {
+  name: 'HomeContent',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+p {
+    color: brown;
+    font-size: 30px;
+}
+</style>
